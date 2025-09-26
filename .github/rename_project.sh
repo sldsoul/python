ec8a2441868b4498767ea78fe2e270b59e5ea9ff
sldsoul <sldsoul@users.noreply.github.com>
2025-09-26 01:10:41 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="sldsoul"
+original_name="python"
+original_urlname="python"
+original_description="Awesome python created by sldsoul"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv python $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ python/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for python."""
+
+from python.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
